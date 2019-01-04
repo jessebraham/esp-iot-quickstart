79d18504c8bbc92f29553ac769c8dd8e98a60d15
Jesse Braham <jesse@beta7.io>
2019-01-03 19:43:15 -0800

    Comment and "finish" up MQTT event loop

--- main/main.c
+++ main/main.c
@@ -27,23 +27,52 @@ static EventGroupHandle_t wifi_event_group = NULL;
 
 esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
 {
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+
     switch (event->event_id)
     {
     case MQTT_EVENT_CONNECTED:
         ESP_LOGI(TAG, "connected to mqtt broker");
         xEventGroupSetBits(mqtt_event_group, CONNECTED_BIT);
+        //
+        // Once a connection has been established to the MQTT broker, you will
+        // likely want to subscribe to one or more topics here.
+        //
+        // eg.) msg_id = esp_mqtt_client_subscribe(client, "/topic/abc", 0);
+        //      ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+        //
         break;
     case MQTT_EVENT_DISCONNECTED:
         ESP_LOGI(TAG, "disconnected from mqtt broker");
         xEventGroupClearBits(mqtt_event_group, CONNECTED_BIT);
+        //
+        // If any long-running routines that publish to the broker are running,
+        // now would be a good time to terminate them.
+        //
         break;
     case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "subscribed to topic, msg_id=%d", event->msg_id);
+        //
+        // Once a topic has been subscribed to, you can publish to that topic,
+        // or simply wait for events to occur.
+        //
+        // eg.) msg_id = esp_mqtt_client_publish(
+        //          client, "/topic/abc", "data", 0, 0, 0
+        //      );
+        //      ESP_LOGI(TAG, "send publish successful, msg_id=%d", msg_id);
+        //
         break;
     case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "unsubscribed from topic, msg_id=%d", event->msg_id);
         break;
     case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "published to topic, msg_id=%d", event->msg_id);
         break;
     case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "data received from topic");
+        ESP_LOGI(TAG, "topic=%.*s\r\n", event->topic_len, event->topic);
+        ESP_LOGI(TAG, "data=%.*s\r\n", event->data_len, event->data);
         break;
     case MQTT_EVENT_ERROR:
         ESP_LOGI(TAG, "mqtt error occurred");
@@ -66,6 +95,7 @@ esp_err_t wifi_event_handler(void *ctx, system_event_t *event)
     case SYSTEM_EVENT_STA_GOT_IP:
         ESP_LOGI(TAG, "received ip address");
         xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+        // once we have an ip address, attempt to connect to the mqtt broker
         connect_to_mqtt_broker(CONFIG_MQTT_BROKER_URI, &mqtt_event_handler);
         break;
     case SYSTEM_EVENT_STA_DISCONNECTED:
